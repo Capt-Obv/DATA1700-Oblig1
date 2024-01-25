e8e3452830ee55733c0436a27e156e41b29597a1
Capt-Obv <49240674+Capt-Obv@users.noreply.github.com>
2024-01-25 13:01:19 +0100

    added the functions velgFilm() og kjopBilett()

---
+++ src/main/resources/static/bestilleKinobilletterScript.js
@@ -0,0 +1,10 @@
+const filmer = [];
+function velgFilm(){
+    let film = document.getElementById("velgFilm");
+    filmer.push(film);
+}
+
+function kjopBilett(){
+    let antall = document.getElementById("antall");
+    if(isNaN(antall))
+}
