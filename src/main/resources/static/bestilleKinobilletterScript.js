159c891dc81ce321656c98a60a1fa328202a73de
Capt-Obv <49240674+Capt-Obv@users.noreply.github.com>
2024-01-25 13:37:19 +0100

    Made the input validation in javascript have to see if i find a better way

--- src/main/resources/static/bestilleKinobilletterScript.js
+++ src/main/resources/static/bestilleKinobilletterScript.js
@@ -3,8 +3,42 @@ function velgFilm(){
     let film = document.getElementById("velgFilm");
     filmer.push(film);
 }
-
+const biletter = [];
 function kjopBilett(){
-    let antall = document.getElementById("antall");
-    if(isNaN(antall))
+    let error = false;
+    let inputAntall = document.getElementById("antall");
+    if(isNaN(inputAntall) || antall < 0){
+        document.getElementById("antallError").innerText = "Antall må være ett positivt nummer!";
+        error = true;
+    }
+    let inputFornavn = document.getElementById("fornavn");
+    if(inputFornavn.length === 0){
+        document.getElementById("fornavnError").innerText="Må skrive noe inn i fornavn";
+        error = true;
+    }
+    let inputEtternavn = document.getElementById("etternavn");
+    if(inputEtternavn.length === 0){
+        document.getElementById("etternavnError").innerText="Må skrive noe inn i etternavn";
+        error = true;
+    }
+    let inputTelefonnr = document.getElementById("telefonnr");
+    if(inputTelefonnr.length === 0){
+        document.getElementById("telfonnrError").innerText="Må skrive noe i telefonnr";
+        error = true
+    }
+    let inputEpost = document.getElementById("epost");
+    if(inputEpost.length === 0){
+        document.getElementById("epostError").innerText="Må skrive noe i epost";
+        error = true;
+    }
+    if(error) return;
+
+    let billet = {
+        antall : inputAntall,
+        fornavn : inputFornavn,
+        etternavn : inputEtternavn,
+        telefonnr : inputTelefonnr,
+        epost : inputEpost
+    };
+    biletter.push(billet);
 }
